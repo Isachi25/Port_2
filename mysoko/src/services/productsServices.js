487ff8633b32a5f69b437904c1c51d389d8e1536
Nicanor Kyamba <nicanorkyamba98@gmail.com>
2024-10-22 12:35:30 +0300

    Retailer dashboard

--- mysoko/src/services/productsServices.js
+++ mysoko/src/services/productsServices.js
@@ -1,10 +1,26 @@
 import { apiBaseUrl } from '../../environment';
 import axios from 'axios';
 
-async function createProduct(product) {
+async function createProduct(retailerId, name, price, availability, category, description, image) {
     try {
-        const response = await axios.post(`${apiBaseUrl}/products`, product);
+        const token = sessionStorage.getItem('accessToken');
+        const formData = new FormData();
+        formData.append('retailerId', retailerId);
+        formData.append('name', name);
+        formData.append('price', price);
+        formData.append('availability', availability);
+        formData.append('category', category);
+        formData.append('description', description);
+        formData.append('image', image);
+
+        const response = await axios.post(`${apiBaseUrl}/products`, formData, {
+            headers: {
+                'Content-Type': 'multipart/form-data',
+                'Authorization': `Bearer ${token}`
+            },
+        });
         return response.data;
+
     } catch (error) {
         console.error('Error:', error);
         throw error;
@@ -13,7 +29,12 @@ async function createProduct(product) {
 
 async function getProductById(id) {
     try {
-        const response = await axios.get(`${apiBaseUrl}/products/${id}`);
+        const token = sessionStorage.getItem('accessToken');
+        const response = await axios.get(`${apiBaseUrl}/products/${id}`, {
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        });
         return response.data;
     } catch (error) {
         console.error('Error:', error);
@@ -23,7 +44,12 @@ async function getProductById(id) {
 
 async function getProducts() {
     try {
-        const response = await axios.get(`${apiBaseUrl}/products`);
+        const token = sessionStorage.getItem('accessToken');
+        const response = await axios.get(`${apiBaseUrl}/products`, {
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        });
         return response.data;
     } catch (error) {
         console.error('Error:', error);
@@ -33,7 +59,22 @@ async function getProducts() {
 
 async function updateProduct(id, product) {
     try {
-        const response = await axios.put(`${apiBaseUrl}/products/${id}`, product);
+        const { retailerId, name, price, availability, category, description, image } = product;
+        const token = sessionStorage.getItem('accessToken');
+        const formData = new FormData();
+        formData.append('retailerId', retailerId);
+        formData.append('name', name);
+        formData.append('price', price);
+        formData.append('availability', availability);
+        formData.append('category', category);
+        formData.append('description', description);
+        formData.append('image', image);
+        const response = await axios.put(`${apiBaseUrl}/products/${id}`, formData, {
+            headers: {
+                'Content-Type': 'multipart/form-data',
+                'Authorization': `Bearer ${token}`
+            }
+        });
         return response.data;
     } catch (error) {
         console.error('Error:', error);
@@ -43,7 +84,12 @@ async function updateProduct(id, product) {
 
 async function deleteProduct(id) {
     try {
-        const response = await axios.delete(`${apiBaseUrl}/products/${id}`);
+        const token = sessionStorage.getItem('accessToken');
+        const response = await axios.delete(`${apiBaseUrl}/products/${id}`, {
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        });
         return response.data;
     } catch (error) {
         console.error('Error:', error);
@@ -53,7 +99,12 @@ async function deleteProduct(id) {
 
 async function deleteProductPermanently(id) {
     try {
-        const response = await axios.delete(`${apiBaseUrl}/products/delete-permanently/${id}`);
+        const token = sessionStorage.getItem('accessToken');
+        const response = await axios.delete(`${apiBaseUrl}/products/${id}/permanent`, {
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        });
         return response.data;
     } catch (error) {
         console.error('Error:', error);
